0ac20d3959605da77a1f843b3f0a75be065d33c3
Ruslan Extazy <rus.extaz@yandex.ru>
2023-11-07 12:42:02 +0300

    VetAnimalBOT

--- keyboards/keyboards_greeting.py
+++ keyboards/keyboards_greeting.py
@@ -4,7 +4,7 @@ from aiogram import types
 kb_greeting = [
     [InlineKeyboardButton(text="Милашка", url="https://t.me/spectropfobia"),
      InlineKeyboardButton(text="Муж милашки", url="https://t.me/rusextaz")],
-    [InlineKeyboardButton(text="Канал милашек", url="https://t.me/tyt_rus")]
+    [InlineKeyboardButton(text="Канал милашек", url="https://t.me/+1OWfWFlFiBxjMjky")]
 ]
 
 kb_greeting = types.InlineKeyboardMarkup(inline_keyboard=kb_greeting)
